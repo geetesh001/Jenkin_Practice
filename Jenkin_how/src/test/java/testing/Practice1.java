b15bdffac915a3c2a122bfcc9f67ef760363fd5b
papi <papi@geetesh>
2016-06-15 21:22:32 +0530

    1st commit

---
+++ Jenkin_how/src/test/java/testing/Practice1.java
@@ -0,0 +1,12 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	
+	@Test
+	public void scenario1(){
+		System.out.println("This is scenario 1");
+	}
+
+}
---
+++ Jenkin_how/src/test/java/testing/Practice2.java
@@ -0,0 +1,12 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class Practice2 {
+	
+	@Test
+	public void scenario2(){
+		System.out.println("This is scenario 2");
+	}
+
+}
